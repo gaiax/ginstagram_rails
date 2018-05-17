fe65a2089f86cf77c1dfd66b3a1ac9da53f6f172
naoya.ohsaki <naoya.ohsaki@gaiax.com>
2018-05-17 15:54:35 +0900

    トップページを作成しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  # ユーザー登録後のリダイレクト先
+  def after_sign_up_path_for(resource)
+    binding.pry
+    user_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'home#index'
+
+  resources :home
   resources :users
 end
