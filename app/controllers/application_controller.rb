4aeb1ed60819743d56dd0cb4001f7dd105fc3d11
naoya.ohsaki <naoya.ohsaki@gaiax.com>
2018-05-17 16:24:44 +0900

    ユーザー登録やログインした後はマイページにリダイレクトするようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  # ユーザー登録後のリダイレクト先
   def after_sign_up_path_for(resource)
-    binding.pry
-    user_path
+    user_path(resource)
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
   end
 end
