2c83915284d957013240177ddaad71398718552a
naoya.ohsaki <naoya.ohsaki@gaiax.com>
2018-05-18 16:21:30 +0900

    ユーザーの名前も登録できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_up_path_for(resource)
     user_path(resource)
@@ -8,5 +9,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     user_path(resource)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
