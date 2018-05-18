1cc9e73abe28bf56c392b585268db6faa2f93d95
naoya.ohsaki <naoya.ohsaki@gaiax.com>
2018-05-18 17:12:17 +0900

    正規表現を半角英数字混合にしました

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :password, presence: true, format: { with: /\w{8,}/ }
+  VALID_PASSWORD_REGEX = /(((?=.*?[a-z])|(?=.*?[A-Z]))(?=.*?\d))[a-z\dA-Z]{8,}/
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
 end
