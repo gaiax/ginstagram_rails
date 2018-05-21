7d670b45bb42a9dd6b23dbea6a6591a40321dc90
naoya.ohsaki <naoya.ohsaki@gaiax.com>
2018-05-21 10:55:39 +0900

    nameカラムのdefaultを削除した

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20180516064215) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name", default: "", null: false
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
