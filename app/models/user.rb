8a5b183a73ec2a80af5dd6d7858257bc5793d5e5
naoya.ohsaki <naoya.ohsaki@gaiax.com>
2018-05-21 13:25:32 +0900

    screen_nameとemailをユニークになるようなvalidateion追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   VALID_PASSWORD_REGEX = /(((?=.*?[a-z])|(?=.*?[A-Z]))(?=.*?\d))[a-z\dA-Z]{8,}/
   validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
+  validates :screen_name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
