b29e5e0fd6ea3a7d4606bf160df757286e7f8ceb
naoya.ohsaki <naoya.ohsaki@gaiax.com>
2018-05-25 11:20:34 +0900

    postsテーブルを作成しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180521081621) do
+ActiveRecord::Schema.define(version: 20180525020512) do
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
