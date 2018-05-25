736d3a4a79b424dd67d918632b7d2c806f555b60
naoya.ohsaki <naoya.ohsaki@gaiax.com>
2018-05-25 14:21:44 +0900

    spec/factories/image.rbpost_idを削除した

--- spec/factories/image.rb
+++ spec/factories/image.rb
@@ -2,6 +2,5 @@ include ActionDispatch::TestProcess
 FactoryBot.define do
   factory :image do
     image { fixture_file_upload("spec/fixtures/img/sample.jpg", "image/jpg") }
-    post_id 1
   end
 end
