e652e3d3f6ca5a6639438da3a93c4378ce2a2d3c
naoya.ohsaki <naoya.ohsaki@gaiax.com>
2018-05-29 10:35:43 +0900

    デバイステストヘルパーで警告が出たので、修正しました。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
