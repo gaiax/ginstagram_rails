629d69f855f10b25e5b82febf8949247a3934d27
naoya.ohsaki <naoya.ohsaki@gaiax.com>
2018-05-31 15:23:25 +0900

    seeds.rbを開発環境だけで実行できるようにしました

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,36 @@
-5.times do |n|
-  name = Faker::Name.name
-  screen_name = Faker::Twitter.screen_name
-  email = Faker::Internet.email
-  password = Faker::Internet.password(8)
-  password_confirmation = password
-  avatar = Faker::Avatar.image
-  User.create!(
-    name: name,
-    screen_name: screen_name,
-    email: email,
-    avatar: avatar,
-    password: password,
-    password_confirmation: password_confirmation,
-  )
-end
+if Rails.env == 'development'
+  5.times do |n|
+    name = Faker::Name.name
+    screen_name = Faker::Twitter.screen_name
+    email = Faker::Internet.email
+    password = Faker::Internet.password(8)
+    password_confirmation = password
+    avatar = Faker::Avatar.image
+    User.create!(
+      name: name,
+      screen_name: screen_name,
+      email: email,
+      avatar: avatar,
+      password: password,
+      password_confirmation: password_confirmation,
+    )
+  end
 
-10.times do |n|
-  description = Faker::Lorem.sentence
-  user_id = rand(1..5)
-  Post.create!(
-    description: description,
-    user_id: user_id,
-  )
-end
+  10.times do |n|
+    description = Faker::Lorem.sentence
+    user_id = rand(1..5)
+    Post.create!(
+      description: description,
+      user_id: user_id,
+    )
+  end
 
-
-10.times do |n|
-  image = File.open("./public/images/sample.jpg")
-  post_id = n + 1
-  Image.create!(
-    image: image,
-    post_id: post_id,
-  )
+  10.times do |n|
+    image = File.open("./public/images/sample.jpg")
+    post_id = n + 1
+    Image.create!(
+      image: image,
+      post_id: post_id,
+    )
+  end
 end
